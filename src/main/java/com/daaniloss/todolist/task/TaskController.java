3546499f5d1633203ac957b5a13f398f7c305723
Danilo <dsouza.danilo@gmail.com>
2023-10-18 22:20:50 -0300

    Dockerfile deploy

--- src/main/java/com/daaniloss/todolist/task/TaskController.java
+++ src/main/java/com/daaniloss/todolist/task/TaskController.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import java.util.List;
-import org.hibernate.mapping.List;
+//import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
--- src/main/java/com/daaniloss/todolist/task/TaskRepository.java
+++ src/main/java/com/daaniloss/todolist/task/TaskRepository.java
@@ -4,7 +4,7 @@ package com.daaniloss.todolist.task;
 import java.util.List;
 import java.util.UUID;
 
-import org.hibernate.mapping.List;
+//import org.hibernate.mapping.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
